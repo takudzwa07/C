b3624b277249b5b1c8ce794033eaa8f38afe9175
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-14 22:16:27 +0200

    Update settings.js for Beulah

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;CZMWjToI#UcWgxSpc8-_oczSzMlXCyULRuHc5aImOOd-Wcl-U7Gc",
+  OWNER_NUMBER: "+2348105234738", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
