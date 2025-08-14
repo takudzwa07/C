54101a608aac23b55f1b6e45e09129fae8fa90a5
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-14 22:53:58 +0200

    Update settings.js for Alma

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;GJkU0TrK#AxhoTuVMwn5-m9orO1CmZwC87brIK5yiu4wUg-jeWTc",
+  OWNER_NUMBER: "+256708307337", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
