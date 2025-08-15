4be53bc7cd2fecb6edc9b4bff759fe060c5e0f55
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-15 17:05:24 +0200

    Update settings.js for Guwablakk

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;ONEw1JYJ#dQKeVQYZDfCXQNObd38TMaHppJpa3y9ADXpUI5EdkmA",
+  OWNER_NUMBER: "+2348142822202", 
+  PREFIX: ","
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
