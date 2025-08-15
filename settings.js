7062b2e516fd642f5f3cb0c8f05d005c4e175ab8
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-15 12:31:26 +0200

    Update settings.js for BUSAGWA

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;2Z1WwZ4K#NEqC2w8INjaskJuqZG_CM35bnffT75S67-Hlu2_UHB8",
+  OWNER_NUMBER: "+256768847836", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
