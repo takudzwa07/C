b7e8ebe5aaa45c7e69e4cf7c484ce06d4d06ebba
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-15 07:01:12 +0200

    Update settings.js for Chaos

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;vVdFzZQL#-cawA7PVlQrkagx8-wggvEH6MKCAQipINU6tlQbMwaQ",
+  OWNER_NUMBER: "+2347079582341", 
+  PREFIX: "?"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
