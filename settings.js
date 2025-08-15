a7b69c446940eb2b3e5be3d87e849bb594ec871b
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-15 10:08:48 +0200

    Update settings.js for Capcut

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;2Q8CCTjS#Ax1PAoAahWiuFi6HMmVLYyjn4Fn1li0ps3s-gJH8Jpw",
+  OWNER_NUMBER: "+2349118343864", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
