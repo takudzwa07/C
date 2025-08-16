c0142885e54a8f05dbe72b39b84bc1d203fb1448
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-16 23:35:24 +0200

    Update settings.js for MR_UPDATE_DATA

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;KVcC3boS#twJ7i_3BGWL9ePShGRslFO2Y2nYJbZwU3Psj13WY34U",
+  OWNER_NUMBER: "+256708252582", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
