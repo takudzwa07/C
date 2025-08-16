373225a394eda484037eed54593240bdbf276263
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-16 10:44:08 +0200

    Update settings.js for Jarvis

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;WUkHQb7T#Q5aUC7EYHmL7Q4c8iMMsMfXG_XDGUz-A7UzogxMr6SU",
+  OWNER_NUMBER: "2778 553 3648", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
