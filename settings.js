91998c8d81c83f199bdb8a6d534c2509d5a970a8
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-16 18:39:26 +0200

    Update settings.js for Badguy

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;2csXyBYa#GCzX5Ho_yZHiFKW_gXoLSKHTC3HUrzCZGj5ctkKnVhU",
+  OWNER_NUMBER: "+2347065831011", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
