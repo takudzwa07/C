4e76ef94ec9118a308f00c29c01fd9740c19fa22
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-16 17:53:51 +0200

    Update settings.js for Deponi

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;2V1FVSJL#uBVbrqx7knjvS7J6nYlq0H4p1GFU5xftGtREbtytyGY",
+  OWNER_NUMBER: "+237678231229", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
