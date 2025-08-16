be5c07dc57213838f33f320964d7ea5a88d2c78f
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-16 18:19:54 +0200

    Update settings.js for kingm

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;LYcBzbaY#-wf_C133SWy-cCIANL_bTCZ0s6NwcZnRD5bD7rGVHKU",
+  OWNER_NUMBER: "+256760854853", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
